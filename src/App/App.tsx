df491772ae88b1002db5d35cc3dd272e4658cc19
Svetlana Solomatnikova <30310439+Svetk0@users.noreply.github.com>
2024-12-18 22:09:47 +0300

    Deploy stable version (#7)
    * add and set up json server
    
    * add scripts
    
    * check async function
    
    * add server api requests
    
    * fix bugs in api requests
    
    * add styles
    
    * add server response to output
    
    * edit code snippets
    
    * add set up error mode
    
    * edit styles, add btn hover, remove light theme
    
    * fix color highlight bug in editor
    
    * Revert 3 4 feat/task block (#4)
    
    * 4 feat/task block (#3)
    
    * add and set up json server
    
    * add scripts
    
    * check async function
    
    * add server api requests
    
    * fix bugs in api requests
    
    * add styles
    
    * add server response to output
    
    * edit code snippets
    
    * add set up error mode
    
    * edit styles, add btn hover, remove light theme
    
    * fix color highlight bug in editor
    
    * add task block
    
    * add toogle to Tips block
    
    * fix style bugs
    
    * Revert "4 feat/task block (#3)"
    
    This reverts commit 16d1f0f0cc9798035eaf519c746fcf903016cac8.
    
    * 4 feat/task block (#6)
    
    * add task block
    
    * add toogle to Tips block
    
    * fix style bugs

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,25 @@
-//import { useState } from "react";
-import "./App.css";
+import { useState } from "react";
+import Task from "../components/Task/Task";
 import CodeEditor from "../components/CodeEditor/CodeEditor";
+import dt from "../constants/data.json";
+import "./App.scss";
 
 function App() {
-  //const [count, setCount] = useState(0);
-
+  const [errorMode, setErrorMode] = useState<boolean>(false);
   return (
     <>
-      <CodeEditor />
+      <header>
+        <button
+          className={errorMode ? `button_error` : ""}
+          onClick={() => setErrorMode(!errorMode)}
+        >
+          {errorMode ? dt.error_mode.on : dt.error_mode.off}
+        </button>
+      </header>
+      <main>
+        <Task />
+        <CodeEditor errorMode={errorMode} />
+      </main>
     </>
   );
 }
---
+++ src/constants/serverConst.ts
@@ -0,0 +1,6 @@
+
+export const BASE_URL = 'http://localhost:7001'
+
+export const POST = 'POST'
+export const DELETE = 'DELETE'
+export const PUT = 'PUT'
